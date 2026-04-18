563ed94a4950cb63341678ecabb790afa5fcb104
maroeg2-rgb <maroeg2@gmail.com>
2026-04-18 04:26:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-const weddingDate = new Date("may15, 2026 00:00:00").getTime();
+ody{
+  margin:0;
+  font-family:sans-serif;
+  background:#000;
+  color:#fff;
+  text-align:center;
+}
 
-const x = setInterval(function() {
+/* شاشة البداية */
+.cover{
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  background:linear-gradient(#000,#222);
+  cursor:pointer;
+}
 
-  const now = new Date().getTime();
-  const distance = weddingDate - now;
+.cover h1{
+  font-size:40px;
+  color:gold;
+  border:2px solid gold;
+  padding:15px 30px;
+  border-radius:10px;
+}
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+.hidden{display:none}
 
-  document.getElementById("countdown").innerHTML =
-    days + " Days Left ❤️";
+/* الهيرو */
+.hero img{
+  width:90%;
+  max-width:500px;
+  border-radius:20px;
+  margin-top:20px;
+  box-shadow:0 0 30px gold;
+}
 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "It's Wedding Day 🎉";
-  }
-}, 1000);
+.hero h1{
+  font-family:'Great Vibes', cursive;
+  font-size:50px;
+  color:gold;
+}
+
+/* العد */
+.countdown{
+  margin:40px 0;
+  font-size:25px;
+}
+
+/* خريطة */
+.map iframe{
+  width:90%;
+  height:300px;
+  border-radius:15px;
+}
+
+/* RSVP */
+.rsvp input{
+  padding:10px;
+  width:200px;
+}
+
+.rsvp button{
+  padding:10px 20px;
+  background:gold;
+  border:none;
+  margin-top:10px;
